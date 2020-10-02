b9464f295bc7d007495fe793b40818f1bc334e26
Nikolajs Korolkovs <k@nick.tf>
2020-10-02 22:04:23 +0300

    s2: +findFileWithObject

--- src/gitinternals/Main.kt
+++ src/gitinternals/Main.kt
@@ -1,22 +1,39 @@
 package gitinternals
 
+import java.io.File
 import java.io.FileInputStream
 import java.util.zip.InflaterInputStream
 
 
 fun main() {
-    println("Enter git object location:")
-    val fileLocation = readLine()!!
-    printGitObject(fileLocation)
+    println("Enter .git directory location:")
+    val gitFolderLocation = readLine()!!
+    println("Enter git object hash:")
+    val gitObjectHash = readLine()!!
+    val file = findFileWithObject(gitFolderLocation, gitObjectHash)
+    printGitObject(file)
 }
 
-fun printGitObject(fileLocation: String) {
+fun findFileWithObject(gitFolderLocation: String, gitObjectHash: String): File? {
+    return File(gitFolderLocation)
+            .walkTopDown()
+            .associateBy { file -> file.absolutePath }
+            .entries.find { e ->
+                e.key.replace("\\", "")
+                        .contains(gitObjectHash)
+            }?.value
+}
+
+fun printGitObject(file: File?) {
+    if (file == null) return
+
     val nullChar = 0.toChar()
-    val fileInputStream = FileInputStream(fileLocation)
+    val fileInputStream = FileInputStream(file)
     val inflaterInputStream = InflaterInputStream(fileInputStream)
 
     inflaterInputStream.use {
         while (inflaterInputStream.available() > 0) {
+            // TODO: debug this, throws java.io.EOFException: Unexpected end of ZLIB input stream
             val charFromStream = inflaterInputStream.read().toChar()
             val charToPrint = if (charFromStream == nullChar) "\n" else charFromStream
             print(charToPrint)
